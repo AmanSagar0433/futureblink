276346166cee817fbedad5ab9cea8e8bbc340a18
Aman Sagar <aman.0433.sagar@gmail.com>
2025-04-07 15:52:34 +0530

    added react restrict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,12 +11,14 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <Toaster />
-    <div className="h-screen relative">
-      <App />
-    </div>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <Toaster />
+      <div className="h-screen relative">
+        <App />
+      </div>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
